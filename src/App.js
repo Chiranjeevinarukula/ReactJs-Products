6fbab4c803e334ec2ef32fd0c34020be2d8d4fd0
venkatesh2344 <100852059+venkatesh2344@users.noreply.github.com>
2024-10-28 19:40:53 +0530

    backend integration (#2)

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 import { Products } from './components/Product/Products';
-
-const products = [
-  {
-    product_id: 2,
-    name: "William T-shirt",
-    price: "3000₹",
-    stockAvailable: true,
-    description: "A Black T-shirt"
-  },
-  {
-    product_id: 3,
-    name: "Utility Jacket",
-    price: "1100₹",
-    stockAvailable: true,
-    description: "A versatile jacket for all seasons."
-  },
-  {
-    product_id: 4,
-    name: "Leather Boots",
-    price: "1500₹",
-    stockAvailable: true,
-    description: "Stylish boots for any occasion."
-  },
-  {
-    product_id: 5,
-    name: "Leather Boots",
-    price: "1500₹",
-    stockAvailable: true,
-    description: "Stylish boots for any occasion."
-  }
-];
+const response = await axios.get('http://127.0.0.1:8080/product/');
+const {data} = response
 
 const App = () => {
   return (
     <div className="App">
       <h1>App Component</h1>
       <div className='products-container'>
-        {products.map((product) => (
+        {data.map((product) => (
           <Products product={product} key={product.product_id} />
         ))}
       </div>
